1fadff4716e0904dc87c0187081c15d90cbfb730
Grisha Angelov <grisha.angelov89@gmail.com>
2024-11-03 09:39:08 +0200

    Add a data provider based on the JSON Server API

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { Admin } from "react-admin";
-import { Layout } from "./Layout";
+import {
+  Admin,
+  Resource,
+  ListGuesser
+} from "react-admin"
+import { Layout } from "./Layout"
+import { dataProvider } from "./dataProvider"
 
-export const App = () => <Admin layout={Layout}></Admin>;
+export const App = () => (
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
+  </Admin>
+)
