c873ef69fe13659b3a95e47d5563e15bb964543a
Grisha Angelov <grisha.angelov89@gmail.com>
2024-11-03 09:45:21 +0200

    Replace ListGuesser with the provided list component from the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import {
   Admin,
   Resource,
-  ListGuesser
+  Datagrid,
+  List,
+  ReferenceField,
+  TextField
 } from "react-admin"
 import { Layout } from "./Layout"
 import { dataProvider } from "./dataProvider"
 
+const PostList = () => (
+  <List>
+    <Datagrid>
+      <ReferenceField source="userId" reference="users" />
+      <TextField source="id" />
+      <TextField source="title" />
+      <TextField source="body" />
+    </Datagrid>
+  </List>
+)
+
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={ListGuesser} />
+    <Resource name="posts" list={PostList} />
   </Admin>
 )
