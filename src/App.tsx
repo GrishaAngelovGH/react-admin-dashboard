2f507333ebe4dc7cdd1f7bc4979fe4406d2815c6
Grisha Angelov <grisha.angelov89@gmail.com>
2024-11-05 15:21:11 +0200

    Add user resource visualized by UserList component

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,11 @@ import { Layout } from "./Layout"
 import { dataProvider } from "./dataProvider"
 
 import PostList from "./components/PostList"
+import UserList from "./components/UserList"
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} />
+    <Resource name="users" list={UserList} />
   </Admin>
 )
