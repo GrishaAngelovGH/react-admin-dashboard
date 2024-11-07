9271adcd9218306af5c6bb4e40611b1b7508993d
Grisha Angelov <grisha.angelov89@gmail.com>
2024-11-07 13:54:02 +0200

    Replace EditGuesser with the provided PostEdit component

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,11 @@ import UserList from "./components/UserList"
 import PostShow from "./components/PostShow"
 import UserShow from "./components/UserShow"
 
+import PostEdit from "./components/PostEdit"
+
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} show={PostShow} edit={EditGuesser} />
+    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} />
     <Resource name="users" list={UserList} show={UserShow} />
   </Admin>
 )
