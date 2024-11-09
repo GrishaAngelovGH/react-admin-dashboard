16ec933cde57e9612d9969490ea50d85c82f4ae4
Grisha Angelov <grisha.angelov89@gmail.com>
2024-11-09 13:06:11 +0200

    Integrate PostCreate component

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ import UserList from "./components/UserList"
 import PostShow from "./components/PostShow"
 import UserShow from "./components/UserShow"
 
+import PostCreate from "./components/PostCreate"
+
 import PostEdit from "./components/PostEdit"
 import UserEdit from "./components/UserEdit"
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} show={PostShow} edit={PostEdit} />
+    <Resource name="posts" list={PostList} show={PostShow} create={PostCreate} edit={PostEdit} />
     <Resource name="users" list={UserList} show={UserShow} edit={UserEdit} />
   </Admin>
 )
