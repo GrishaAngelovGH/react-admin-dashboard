321f50968e4edbf00ffe96a6dff4f32536b4e225
Grisha Angelov <grisha.angelov89@gmail.com>
2025-06-06 13:56:19 +0300

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['@babel/plugin-transform-private-methods']
+      }
+    })
+  ],
   server: {
     host: true,
   },
@@ -19,32 +25,32 @@ export default defineConfig(({ mode }) => ({
 
 function getAliasesToDebugInProduction() {
   return [
-  { find: "react-admin", replacement: path.resolve(__dirname, "./node_modules/react-admin/src") },
-  { find: "ra-core", replacement: path.resolve(__dirname, "./node_modules/ra-core/src") },
-  {
-    find: "ra-ui-materialui",
-    replacement: path.resolve(__dirname, "./node_modules/ra-ui-materialui/src"),
-  },
-  {
-    find: "ra-i18n-polyglot",
-    replacement: path.resolve(__dirname, "./node_modules/ra-i18n-polyglot/src"),
-  },
-  {
-    find: "ra-language-english",
-    replacement: path.resolve(__dirname, "./node_modules/ra-language-english/src"),
-  },
-  {
-    find: "ra-data-json-server",
-    replacement: path.resolve(__dirname, "./node_modules/ra-data-json-server/src"),
-  },
-  {
-    find: "ra-data-simple-rest",
-    replacement: path.resolve(__dirname, "./node_modules/ra-data-simple-rest/src"),
-  },
-  {
-    find: "ra-data-fakerest",
-    replacement: path.resolve(__dirname, "./node_modules/ra-data-fakerest/src"),
-  },
-  // add any other react-admin packages you have
-];
+    { find: "react-admin", replacement: path.resolve(__dirname, "./node_modules/react-admin/src") },
+    { find: "ra-core", replacement: path.resolve(__dirname, "./node_modules/ra-core/src") },
+    {
+      find: "ra-ui-materialui",
+      replacement: path.resolve(__dirname, "./node_modules/ra-ui-materialui/src"),
+    },
+    {
+      find: "ra-i18n-polyglot",
+      replacement: path.resolve(__dirname, "./node_modules/ra-i18n-polyglot/src"),
+    },
+    {
+      find: "ra-language-english",
+      replacement: path.resolve(__dirname, "./node_modules/ra-language-english/src"),
+    },
+    {
+      find: "ra-data-json-server",
+      replacement: path.resolve(__dirname, "./node_modules/ra-data-json-server/src"),
+    },
+    {
+      find: "ra-data-simple-rest",
+      replacement: path.resolve(__dirname, "./node_modules/ra-data-simple-rest/src"),
+    },
+    {
+      find: "ra-data-fakerest",
+      replacement: path.resolve(__dirname, "./node_modules/ra-data-fakerest/src"),
+    },
+    // add any other react-admin packages you have
+  ];
 }
